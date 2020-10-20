06aa1a374751bf3e5e69219ab68f35cc7929815f
Aulos Rafael <aulosrafael@gmail.com>
2020-10-20 16:47:45 -0300

    removing flutter dependency

--- test/galois_field_test.dart
+++ test/galois_field_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 import 'package:reed_solomon/src/galois_field.dart';
 
 void main() {
--- test/reed_solomon_test.dart
+++ test/reed_solomon_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 import 'package:reed_solomon/src/reed_solomon.dart';
 import 'package:reed_solomon/src/reed_solomon_exception.dart';
 
